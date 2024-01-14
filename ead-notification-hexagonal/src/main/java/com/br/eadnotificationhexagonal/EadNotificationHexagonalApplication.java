da407f23983db5d0f99d627f22b478efaf2152c5
Aline Paz <aline.soares.2@hotmail.com>
2024-01-14 10:17:20 -0300

    iniciando projeto em hexagonal

--- ead-notification-hexagonal/src/main/java/com/br/eadnotificationhexagonal/EadNotificationHexagonalApplication.java
+++ ead-notification-hexagonal/src/main/java/com/br/eadnotificationhexagonal/EadNotificationHexagonalApplication.java
@@ -2,8 +2,10 @@ package com.br.eadnotificationhexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EadNotificationHexagonalApplication {
 
 	public static void main(String[] args) {
