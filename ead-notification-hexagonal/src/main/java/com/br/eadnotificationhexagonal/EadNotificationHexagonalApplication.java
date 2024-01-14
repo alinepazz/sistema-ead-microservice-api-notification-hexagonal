c872619c8726b482fd302b2213f985169cef775b
Aline Paz <aline.soares.2@hotmail.com>
2024-01-14 10:00:08 -0300

    projeto criado

---
+++ ead-notification-hexagonal/src/main/java/com/br/eadnotificationhexagonal/EadNotificationHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.br.eadnotificationhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadNotificationHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadNotificationHexagonalApplication.class, args);
+	}
+
+}
---
+++ ead-notification-hexagonal/src/test/java/com/br/eadnotificationhexagonal/EadNotificationHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.eadnotificationhexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadNotificationHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
