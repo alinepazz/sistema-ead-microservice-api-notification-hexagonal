cbf8324baab49332b4c7afe7d1e5a4f0206583cf
Aline Paz <aline.soares.2@hotmail.com>
2024-01-14 15:08:44 -0300

    criando o metodo save

---
+++ ead-notification-hexagonal/src/main/java/com/br/eadnotificationhexagonal/adapters/outbound/persistence/NotificationPersistencePortImpl.java
@@ -0,0 +1,40 @@
+package com.br.eadnotificationhexagonal.adapters.outbound.persistence;
+
+import com.br.eadnotificationhexagonal.adapters.outbound.persistence.entities.NotificationEntity;
+import com.br.eadnotificationhexagonal.core.domain.NotificationDomain;
+import com.br.eadnotificationhexagonal.core.domain.PageInfo;
+import com.br.eadnotificationhexagonal.core.domain.enums.NotificationSatus;
+import com.br.eadnotificationhexagonal.core.ports.NotificationPersistencePort;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class NotificationPersistencePortImpl implements NotificationPersistencePort {
+
+    private final NotificationJpaRepoository notificationJpaRepoository;
+    private final ModelMapper modelMapper;
+
+    public NotificationPersistencePortImpl(NotificationJpaRepoository notificationJpaRepoository, ModelMapper modelMapper) {
+        this.notificationJpaRepoository = notificationJpaRepoository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public NotificationDomain save(NotificationDomain notificationDomain) {
+        NotificationEntity entity = notificationJpaRepoository.save(modelMapper.map(notificationDomain, NotificationEntity.class));
+        return modelMapper.map(entity, NotificationDomain.class);
+    }
+
+    @Override
+    public List<NotificationDomain> findAllByUserIdAndNotificationStatus(UUID userId, NotificationSatus notificationSatus, PageInfo pageInfo) {
+        return null;
+    }
+
+    @Override
+    public Optional<NotificationDomain> findByNotificationIdAndUserId(UUID notificationId, UUID userId) {
+        return Optional.empty();
+    }
+}
